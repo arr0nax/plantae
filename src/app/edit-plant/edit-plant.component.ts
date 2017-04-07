ac381f2e2050acb6bd6ae474894e4f1c54cd2699
clayton collins <claytonccollins@gmail.com>
2017-04-07 14:50:08 -0700

    add icon and continue update styling - edit form more readable

--- src/app/edit-plant/edit-plant.component.ts
+++ src/app/edit-plant/edit-plant.component.ts
@@ -14,6 +14,7 @@ export class EditPlantComponent implements OnInit {
   constructor(private plantService: PlantService, private router: Router) { }
 
   ngOnInit() {
+    console.log(this.plant);
   }
 
   editPlant(plant) {
